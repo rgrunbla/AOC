2959e879bc0c1ecc1f813ecfc336197ca632b712
Rémy Grünblatt <remy@grunblatt.org>
2020-12-08 09:44:40 +0100

    First day 07

--- 2020/07/rust/first/src/main.rs
+++ 2020/07/rust/first/src/main.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 use regex::Regex;
+use std::collections::HashMap;
+use std::collections::HashSet;
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
@@ -16,28 +18,44 @@ fn split_once(in_string: &str, car: char) -> (&str, &str) {
     (first, second)
 }
 
-type Pair<'a> = (i32, &'a str);
-
-fn parse_line(line: &str) { //-> (&str, Pair) {
-    // wavy gray bags contain 2 mirrored fuchsia bags, 2 mirrored chartreuse bags, 3 mirrored blue bags.
+fn parse_line<'a>(line: &'a str, graph: &mut HashMap<String, Vec<String>>) {
+    // light chartreuse bags contain 1 mirrored yellow bag, 2 vibrant violet bags.
     let container = Regex::new(r"^([A-Za-z ]*) bags contain ").unwrap();
-    let contained = Regex::new(r"(\d+) ([A-Za-z ]*) bags").unwrap();
-    println!("{}", &container.captures(line).unwrap()[1]);
-    for _match in container.captures_iter(line) {
-        println!("{}, {}", &_match[1], &_match[2]);
+    let container =  &container.captures(line).unwrap()[1];
+    let contained = Regex::new(r"(\d+) ([A-Za-z ]*) bag").unwrap();
+
+    for _match in contained.captures_iter(line) {
+        graph.entry(_match[2].to_string()).or_insert(vec![container.to_string()]).push(container.to_string());
     }
 }
 
 fn main() {
+    let mut graph = HashMap::new();
+
     let filename = "/home/remy/AOC/2020/07/input";
 
     if let Ok(lines) = read_lines(filename) {
         for line in lines {
             if let Ok(line) = line {
-                parse_line(&line);
-                
+                parse_line(&line, &mut graph);   
             }
         }
+        let mut set = HashSet::new();
+        let mut size = 0;
+        set.insert(String::from("shiny gold"));
+        loop {
+            if set.len() == size {
+                break;
+            }
+            size = set.len();
+            for key in set.clone() {
+                match graph.get(&key) {
+                    Some(item) => for color in item { set.insert(color.to_string()); },
+                    None => println!("{} has no parent", key)
+                }
+            }
+        }
+        println!("Set has size {}, result: {}", set.len(), set.len()-1);
     } else {
         println!("Error");
     }
